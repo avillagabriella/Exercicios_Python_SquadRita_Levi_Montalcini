2deb5662558cc69e835f0ba3a95c0e892aaf126b
Melissa Gasque <melissagasque.com>
2024-01-22 20:10:43 -0300

    atividades 3 e 4 lista conceitos-básicos

---
+++ lista_exercicio_conceito_/questao3.py
@@ -0,0 +1,5 @@
+quilometros = int(input("Quantos quilometros(km)? "))
+metros = quilometros*1000
+centimetros = quilometros*100000
+milimetros = centimetros*10
+print(f"{quilometros}km equivale a {metros}m ou {centimetros}cm ou {milimetros}mm")
---
+++ lista_exercicio_conceito_/questao4.py
@@ -0,0 +1,4 @@
+distancia = int(input("Distancia percorrida(Km) "))
+litros = int(input("Combustível consumido(L) "))
+consumo = distancia/litros
+print(f"O consumo médio de gasolina é de: {consumo}Km/L")
