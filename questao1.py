96be577d9ebb4c01aa47e2c329f8be883d0eff9f
[NaHaze] <[nat.abarno@gmail.com]>
2024-01-22 20:58:54 -0300

    Exercício tomada decisao - Questão 1

---
+++ questao1.py
@@ -0,0 +1,9 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+if num1 > num2:
+    print(f'O número {num1} é o maior número')
+else: 
+    print(f'O número {num2} é o maior número')
+
+
