1249c5978571aa07ad242574908b607edf14a531
avillagabriella <avillagabriella@outlook.com>
2024-01-22 19:37:30 -0300

    Questão 1 lista conceito

---
+++ questao1.py
@@ -0,0 +1,13 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+
+soma = num1 + num2 + num3
+subtracao = num1 - num2 - num3
+multiplicacao = num1 * num2 * num3
+divisao = num1 / num2 / num3
+
+print("Soma:", soma)
+print("Subtração:", subtracao)
+print("Multiplicação:", multiplicacao)
+print("Divisão:", divisao)
