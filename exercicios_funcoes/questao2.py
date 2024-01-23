d5c244f60c3d015cbb1e65f89f5364307cd631f0
karenribeiro7 <kevr@discente.ifpe.edu.br>
2024-01-22 22:22:04 -0300

    add questao 2 da lista de funcoes

---
+++ exercicios_funcoes/questao2.py
@@ -0,0 +1,8 @@
+def inverter (numero):
+    numeroInvertido = numero[::-1]
+    print(f'O numero invertido é: {numeroInvertido}')
+
+
+num1 = input('Digite o numero a ser invertido: ')
+
+inverter(num1)
