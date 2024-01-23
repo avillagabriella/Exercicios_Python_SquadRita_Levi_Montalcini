664b214d5aef2c7b7b3f1c360262867ed7ef1a0e
YaraDanieleUchoa <yaradanieleuchoa@gmail.com>
2024-01-22 22:13:00 -0300

    adição da pasta com nome da lista de exercicios e coloquei as atividades dentro dela

---
+++ exercicios_funcoes/questao1.py
@@ -0,0 +1,9 @@
+def soma(n1,n2,n3):
+    somar = n1 + n2 + n3
+    print(f'A soma dos valores inseridos é: {somar}')
+
+num1 = int(input('Primeiro número: '))
+num2 = int(input('Segundo número: '))
+num3 = int(input('terceiro número: '))
+
+soma(num1,num2,num3)
