c24407c728a368ce350f2af9181c65254a1fede9
Ana-Almeida110 <coiadoalmeida@gmail.com>
2024-01-23 21:44:07 -0300

    adição das questões 4 e 6 da lista Lista, Tuplas e Dicionários

---
+++ questao4.py
@@ -0,0 +1,16 @@
+contatos = {
+    'Manoel Souza': '99999-9999',
+    'Cláudia Pinheiro': '98754-6398',
+    'Carlos Machado': '8547-9658',
+    'Jose Roberto Carvalho': '8974-6985',
+}
+
+def procurar_contato(nome):
+    if nome in contatos:
+        return f"Nome: {nome} - telefone: {contatos[nome]}"
+    else:
+        return f"Contato não encontrado {nome}"
+
+nome_pesquisa = input("Digite o nome: ")
+ 
+resultado = procurar_contato(nome_pesquisa)
